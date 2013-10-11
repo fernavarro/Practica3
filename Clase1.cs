8bef2c9c6672e0b13cb8669de3301fe3f0559cdc
fernavarro <fernandho0@hotmail.com>
2013-10-10 19:48:13 -0600

    inicio

---
+++ Clase1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace tareaa
+{
+	/// <summary>
+	/// Description of Persona.
+	/// </summary>
+	public class Persona
+	{
+		public int Codigo;
+		public String Nombre;
+		public String Telefono;
+		public String Facebook;
+		public Persona(){
+
+		}
+	}
+}
